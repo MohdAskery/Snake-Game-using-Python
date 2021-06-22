b36caad07c33810bf9ae321851e70e63c72581b3
Mohd Askery <mohdaskery7780@gmail.com>
2021-06-22 12:04:39 +0530

    Version 0.3

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ import time
 # colors
 # ========================================================
 
-white = (255, 255, 255)
-red = (255, 0, 0)
-black = (0, 0, 0)
+white =(44,62,78,1)
+# background-image: linear-gradient( 76.3deg,  rgba(44,62,78,1) 12.6%, rgba(69,103,131,1) 82.8% );
+red =(255,61,89,1)
+black = (229,251,31,1)
+yellow=(155,254,23,1)
+green=(173,255,47)
+
+
+
 
 # ========================================================
 
@@ -16,10 +22,10 @@ screen_height = 600
 
 pygame.init()
 
-gameWindow = pygame.display.set_mode((screen_width, screen_height))
+gameWindow = pygame.display.set_mode((screen_width, screen_height),)
 pygame.display.set_caption("Snake Game")
-pygame.display.update()
 
+pygame.display.update()
 
 clock = pygame.time.Clock()
 
@@ -37,25 +43,31 @@ def gameloop():
     # Game specific variables
     exit_game = False
     game_over = False
-    snake_x = 70
-    snake_y = 80
+    snake_x = random.randint(70,140)
+    snake_y = random.randint(100,180)
     snake_size = 20
-    fps = 60
+    fps = 120
     velocity_x = 0
     velocity_y = 0
-    food_x = random.randint(70, screen_width-100)
-    food_y = random.randint(70, screen_width-100)
+    food_x = random.randint(80, screen_width/1.5)
+    food_y = random.randint(80,screen_height/1.5)
+    
     score = 0
     inital_velocity = 5
 
     snk_list = []
     snk_length = 1
     
+    with open("highScore.txt","r") as f:
+        highScore=f.read()
+        
     while not exit_game:
-   
+        
         if game_over:
             # gameWindow.fill(white)
-            text_screen("Game Over! Press Enter To continue", red, 120, 260)
+            with open("highScore.txt","w") as f:
+                f.write(str(highScore))
+            text_screen("Game Over! Press Enter To continue", green, 120, 260)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit_game = True
@@ -80,21 +92,25 @@ def gameloop():
                         velocity_y = inital_velocity
                         velocity_x = 0
             if(abs(snake_x-food_x) < 12 and abs(snake_y-food_y) < 12):
-                score += 1
-                food_x = random.randint(20, screen_width/2)
-                food_y = random.randint(20, screen_height/2)
+                score += 10
+                food_x = random.randint(200, screen_width-10)
+                food_y = random.randint(80,screen_height-100)
                 snk_length += 5
-                inital_velocity+=0.5
+            
+                if(score>int(highScore)):
+                    highScore=score
+                    
+
                 
             gameWindow.fill(white)
         
             
-            text_screen("Speed: " + str(int(inital_velocity)), red, screen_width-200, 0)
-            text_screen("Score: " + str(score * 10), red, 5, 5)
+            text_screen("High score : " + str(highScore), red, screen_width-300, 0)
+            text_screen("Score: " + str(score), red, 5, 5)
             
             pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
             # pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
-            pygame.draw.line(gameWindow, red, (screen_width,60), (0,60), width=3)
+            pygame.draw.line(gameWindow, yellow, (screen_width,60), (0,60), width=3)
             snake_x += velocity_x
             snake_y += velocity_y
             head = []
